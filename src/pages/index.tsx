b175622546280b37440372a366669dfadea4509f
LinhDan <nhlinhdan.tc@gmail.com>
2024-11-19 11:03:51 +0700

    homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import Page from "@/components/Layout/Page";
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
     <Page className="flex items-center justify-center min-h-screen">
-      <section className="flex flex-col gap-y-5 my-24 lg:my-0"></section>
+      <section className="flex flex-col gap-y-5 my-24 lg:my-0 items-center">
+        <p className="text-7xl font-bold text-white">Welcome to Kaohut</p>
+        <p className="text-white">
+          An interactive learning platform that makes education fun and engaging
+          for both teachers and students
+        </p>
+        <button className="bg-white flex items-center p-3 rounded-full pl-4 text-purple">
+          <p className="font-semibold">Get Started</p> <ChevronRight />
+        </button>
+      </section>
     </Page>
   );
 }
