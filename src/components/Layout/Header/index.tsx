f753ac2f10fa02abb1758ffd024cd597cc218a76
Peter Pham <pttp15122002@gmail.com>
2024-11-25 08:19:30 +0700

    style: relocate logo and navbar into 1 div

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,12 +1,16 @@
 import ThemeToggle from "@/components/Theme/ThemeToggle";
 import Sidebar from "../Sidebar";
 import FullLogo from "../Logo/FullLogo";
+import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <header className="p-5 max-w-[100rem] mx-auto bg-white">
+    <header className="p-5 max-w-[100rem] mx-auto bg-white flex items-center">
       <Sidebar />
-      <FullLogo />
+      <div className="flex items-center gap-x-7">
+        <FullLogo />
+        <NavBar />
+      </div>
     </header>
   );
 }
