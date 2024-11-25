9ce923ae4522cbc686fe6cae2f9a57e1b8c432a0
Peter Pham <pttp15122002@gmail.com>
2024-11-25 11:06:31 +0700

    feat: use new Actions component

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -1,35 +1,17 @@
-import {
-  HeroHighlight,
-  Highlight,
-} from "@/components/aceternity/HeroHighlight";
-import { motion } from "framer-motion";
 import Image from "next/image";
+import HighlightedTitle from "./HighlightedTitle";
+import Actions from "./Actions";
 
 export default function Hero() {
   return (
     <section className="flex w-full items-center">
-      <div className="w-7/12">
-        <HeroHighlight className="w-full">
-          <motion.h1
-            initial={{
-              opacity: 0,
-              y: 20,
-            }}
-            animate={{
-              opacity: 1,
-              y: [20, -5, 0],
-            }}
-            transition={{
-              duration: 0.5,
-              ease: [0.4, 0.0, 0.2, 1],
-            }}
-            className="text-7xl leading-normal cursor-default font-extrabold text-primary"
-          >
-            Engage, Learn, and Have Fun Together with is a{" "}
-            <Highlight className="text-white">Kaohut!</Highlight>
-          </motion.h1>
-        </HeroHighlight>
+      <div className="w-7/12 flex flex-col gap-y-5">
         <HighlightedTitle />
+        <p className="text-lg">
+          Transform the way you connect and learn through interactive quizzes,
+          polls, and games—all in one dynamic platform.
+        </p>
+        <Actions />
       </div>
       <Image
         src={"/images/hero.png"}
