12aa984d392de416b9e0982cfca6cda0bab9986d
Peter Pham <pttp15122002@gmail.com>
2024-11-25 11:25:38 +0700

    style: responsive for tablets

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -4,10 +4,10 @@ import Actions from "./Actions";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col gap-y-7 lg:flex-row w-full lg:items-center py-12 lg:py-0">
-      <div className="w-full lg:w-7/12 flex flex-col gap-y-5 items-center lg:items-start">
+    <section className="flex flex-col gap-y-7 lg:flex-row w-full md:items-center py-12 lg:py-0">
+      <div className="w-full md:w-9/12 lg:w-7/12 flex flex-col gap-y-5 items-center lg:items-start">
         <HighlightedTitle />
-        <p className="lg:text-lg text-center lg:text-left">
+        <p className="md:text-lg lg:text-xl text-center lg:text-left">
           Transform the way you connect and learn through interactive quizzes,
           polls, and games—all in one dynamic platform.
         </p>
@@ -19,7 +19,7 @@ export default function Hero() {
         width={0}
         height={0}
         sizes="100vw"
-        className="w-full lg:w-5/12 h-auto"
+        className="w-full md:w-1/2 lg:w-5/12 h-auto"
       />
     </section>
   );
