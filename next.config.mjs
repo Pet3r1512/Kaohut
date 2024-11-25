ba06949f7274928dd8ee8d8fc91825dc4530e90f
Peter Pham <pttp15122002@gmail.com>
2024-11-25 14:35:20 +0700

    feat: add i18next to Nextjs app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import pkg from "./next-i18next.config.js";
+const { i18n } = pkg;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["vi", "en"],
+    defaultLocale: "vi",
+  },
   images: {
     remotePatterns: [
       {
