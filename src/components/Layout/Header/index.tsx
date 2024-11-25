cd81f6be54f7e6b759ea35b3742e54e33b7c4bc6
Peter Pham <pttp15122002@gmail.com>
2024-11-25 09:58:19 +0700

    style: hide logo in large screens

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     <section className="sticky top-0 z-30 !backdrop-filter !backdrop-blur-xl flex justify-center lg:h-24">
       <header className="p-5 max-w-[100rem] mx-auto flex items-center justify-between fixed w-full">
         <Sidebar />
-        <FullLogo />
+        <FullLogo className="lg:hidden" />
         <div className="lg:flex items-center gap-x-7 hidden">
           <FullLogo />
           <NavBar />
