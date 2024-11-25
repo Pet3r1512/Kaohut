4cfbceda2ac0b347821f0aa7fc264c9c9ed391e9
Peter Pham <pttp15122002@gmail.com>
2024-11-25 09:42:58 +0700

    refactor

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -5,13 +5,15 @@ import Settings from "./Settings";
 
 export default function Header() {
   return (
-    <header className="p-5 max-w-[100rem] mx-auto flex items-center justify-between">
-      <Sidebar />
-      <div className="flex items-center gap-x-7">
-        <FullLogo />
-        <NavBar />
-      </div>
-      <Settings />
-    </header>
+    <section className="sticky top-0 z-30 !backdrop-filter !backdrop-blur-xl flex justify-center lg:h-24">
+      <header className="p-5 max-w-[100rem] mx-auto flex items-center justify-between fixed w-full">
+        <Sidebar />
+        <div className="flex items-center gap-x-7">
+          <FullLogo />
+          <NavBar />
+        </div>
+        <Settings />
+      </header>
+    </section>
   );
 }
