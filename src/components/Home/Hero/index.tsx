eeb87fdc77dd749bfac808c85dc9c2f7c3d3fc44
Peter Pham <pttp15122002@gmail.com>
2024-11-25 10:50:44 +0700

    feat: use hero-highlight component

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -1,12 +1,34 @@
+import {
+  HeroHighlight,
+  Highlight,
+} from "@/components/aceternity/HeroHighlight";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="flex w-full ">
-      <div className="w-2/3">
-        <p className="text-7xl font-extrabold text-primary leading-tight">
-          Engage, Learn, and Have Fun Together with Kaohut!
-        </p>
+    <section className="flex w-full items-center">
+      <div className="w-7/12">
+        <HeroHighlight className="w-full">
+          <motion.h1
+            initial={{
+              opacity: 0,
+              y: 20,
+            }}
+            animate={{
+              opacity: 1,
+              y: [20, -5, 0],
+            }}
+            transition={{
+              duration: 0.5,
+              ease: [0.4, 0.0, 0.2, 1],
+            }}
+            className="text-7xl leading-normal cursor-default font-extrabold text-primary"
+          >
+            Engage, Learn, and Have Fun Together with is a{" "}
+            <Highlight className="text-white">Kaohut!</Highlight>
+          </motion.h1>
+        </HeroHighlight>
       </div>
       <Image
         src={"/images/hero.png"}
