6be27596f16c1404cb05a8502ee717f8234f5b8d
Peter Pham <pttp15122002@gmail.com>
2024-11-28 10:57:34 +0700

    ops: convert to vite template

--- src/components/Layout/Header/Sidebar.tsx
+++ src/components/Layout/Header/Sidebar.tsx
@@ -1,17 +1,16 @@
-import ThemeToggle from "@/components/Theme/ThemeToggle";
+import ThemeToggle from "@/components/ThemeToggle";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { Menu } from "lucide-react";
-import Link from "next/link";
 
 export default function Sidebar() {
   return (
     <Drawer direction="left">
-      <DrawerTrigger className="lg:hidden">
+      <DrawerTrigger className="lg:hidden relative top-8 left-5">
         <Menu />
       </DrawerTrigger>
       <DrawerContent className="h-[100dvh] w-2/3 !rounded-l-none rounded-r-xl px-5 py-12 flex flex-col gap-y-5">
         <ThemeToggle />
-        <Link
+        <a
           href="https://github.com/Pet3r1512/DevIniter_CLI/tree/master/templates/nextjs"
           className="size-8 lg:size-10 rounded-full bg-gray-500 dark:bg-gray-200 p-2"
         >
@@ -25,7 +24,7 @@ export default function Sidebar() {
             <g
               id="SVGRepo_tracerCarrier"
               strokeLinecap="round"
-              strokeLinejoin="round"
+              stroke-linejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
               {" "}
@@ -58,7 +57,7 @@ export default function Sidebar() {
               </g>{" "}
             </g>
           </svg>
-        </Link>
+        </a>
       </DrawerContent>
     </Drawer>
   );
--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -41,7 +41,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950",
         className,
       )}
       {...props}
@@ -95,7 +95,7 @@ const DrawerDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
+    className={cn("text-sm text-neutral-500 dark:text-neutral-400", className)}
     {...props}
   />
 ));
