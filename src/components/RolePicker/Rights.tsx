60599b297b7454ee0c8969236dde381e1abbeaea
Linh Dan <118578840+nguyenholinhdan@users.noreply.github.com>
2024-11-29 16:57:00 +0700

    #FEAT-017: Teacher rights (#22)
    * fix: wrong role
    
    * doc: update teacher's rights
    
    * feat: display rights with icons

--- src/components/RolePicker/Rights.tsx
+++ src/components/RolePicker/Rights.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { CircleCheckBig } from "lucide-react";
 
 export default function Rights({
   role,
@@ -10,23 +11,51 @@ export default function Rights({
   const info = [
     {
       role: "teacher",
-      rights: ["Create Quizzes", "Host a Game"],
+      rights: [
+        "Create, Publish and Save Quizzes",
+        "Host a Game",
+        "Access to other Quizzes",
+      ],
     },
     {
-      role: "teacher",
+      role: "student",
       rights: ["Join game"],
     },
   ];
 
   return (
-    <div className={cn(className)}>
-      {role === "teacher"
-        ? info[0].rights.map((right, index) => {
-            return <p key={index}>{right}</p>;
-          })
-        : info[1].rights.map((right, index) => {
-            return <p key={index}>{right}</p>;
-          })}
+    <div className={cn("flex flex-col gap-y-8", className)}>
+      <p className="lg:text-4xl font-bold">
+        What can you do as a{" "}
+        <span>{role === "teacher" ? "Teacher" : "Student"}</span>
+      </p>
+      <ul className="flex flex-col gap-y-1.5 text-xl font-semidol">
+        {role === "teacher"
+          ? info[0].rights.map((right, index) => {
+              return (
+                <p className="flex items-center gap-x-1" key={index}>
+                  <CircleCheckBig
+                    size={18}
+                    className="text-green-600 mt-0.5"
+                    strokeWidth={3}
+                  />
+                  {right}
+                </p>
+              );
+            })
+          : info[1].rights.map((right, index) => {
+              return (
+                <p className="flex items-center gap-x-1" key={index}>
+                  <CircleCheckBig
+                    size={18}
+                    className="text-green-600 mt-0.5"
+                    strokeWidth={3}
+                  />
+                  {right}
+                </p>
+              );
+            })}
+      </ul>
     </div>
   );
 }
