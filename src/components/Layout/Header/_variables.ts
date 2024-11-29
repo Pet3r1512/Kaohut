af20036f24b1b93eaaa2407f78430707ed983318
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-11-29 07:28:06 +0700

    #FEAT-012: build navigation bar (#13)
    * feat: move Navs info into seperated file
    
    * feat: header with navbar only
    
    * feat: navigation bar

---
+++ src/components/Layout/Header/_variables.ts
@@ -0,0 +1,14 @@
+export const Navs = [
+  {
+    name: "About Us",
+    href: "/about",
+  },
+  {
+    name: "Features",
+    href: "/features",
+  },
+  {
+    name: "Pricing",
+    href: "/pricing",
+  },
+];
