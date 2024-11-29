2e33f871caf9bed53b58283faeb052ac0ccb1f50
Peter Pham <pttp15122002@gmail.com>
2024-11-29 19:09:49 +0700

    ops: add core config for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,5 +12,8 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
+  core: {
+    builder: "@storybook/builder-vite", // 👈 The builder enabled here.
+  },
 };
 export default config;
