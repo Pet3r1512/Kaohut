b6007401f518692a04ff255011b59b989317277f
Peter Pham <pttp15122002@gmail.com>
2024-11-29 19:38:48 +0700

    test: stories for FullLogo component

--- src/components/Layout/Logos/FullLogo.stories.ts
+++ src/components/Layout/Logos/FullLogo.stories.ts
@@ -1,8 +1,14 @@
-import type { Meta } from "@storybook/web-components";
+import type { Meta, StoryObj } from "@storybook/react";
+import FullLogo from "./FullLogo";
 
-const meta: Meta = {
-  title: "Full Logo",
-  component: "FullLogo",
+const meta: Meta<typeof FullLogo> = {
+  component: FullLogo,
 };
 
 export default meta;
+
+type Story = StoryObj<typeof FullLogo>;
+
+export const Primary: Story = {
+  args: {},
+};
