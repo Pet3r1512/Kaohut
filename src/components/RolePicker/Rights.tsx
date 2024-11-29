1217775700de2b8480359ba69b95a1ccbadc70d4
LinhDan <linhdan.sam@gmail.com>
2024-11-29 16:53:00 +0700

    feat: display rights with icons

--- src/components/RolePicker/Rights.tsx
+++ src/components/RolePicker/Rights.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { CircleCheckBig } from "lucide-react";
 
 export default function Rights({
   role,
@@ -23,14 +24,38 @@ export default function Rights({
   ];
 
   return (
-    <div className={cn(className)}>
-      {role === "teacher"
-        ? info[0].rights.map((right, index) => {
-            return <p key={index}>{right}</p>;
-          })
-        : info[1].rights.map((right, index) => {
-            return <p key={index}>{right}</p>;
-          })}
+    <div className={cn("flex flex-col gap-y-8", className)}>
+      <p className="lg:text-4xl font-bold">
+        What can you do as a{" "}
+        <span>{role === "teacher" ? "Teacher" : "Student"}</span>
+      </p>
+      <ul className="flex flex-col gap-y-1.5 text-xl font-semidol">
+        {role === "teacher"
+          ? info[0].rights.map((right, index) => {
+              return (
+                <p className="flex items-center gap-x-1" key={index}>
+                  <CircleCheckBig
+                    size={18}
+                    className="text-green-600 mt-0.5"
+                    strokeWidth={3}
+                  />
+                  {right}
+                </p>
+              );
+            })
+          : info[1].rights.map((right, index) => {
+              return (
+                <p className="flex items-center gap-x-1" key={index}>
+                  <CircleCheckBig
+                    size={18}
+                    className="text-green-600 mt-0.5"
+                    strokeWidth={3}
+                  />
+                  {right}
+                </p>
+              );
+            })}
+      </ul>
     </div>
   );
 }
