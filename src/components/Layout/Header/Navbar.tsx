fb6c25026158a73d3094701f143a3543a717e5c9
Peter Pham <pttp15122002@gmail.com>
2024-11-29 07:54:54 +0700

    imp: use translations on navbar's items

--- src/components/Layout/Header/Navbar.tsx
+++ src/components/Layout/Header/Navbar.tsx
@@ -1,6 +1,9 @@
+import { useTranslation } from "react-i18next";
 import { Navs } from "./_variables";
 
 export default function NavBar() {
+  const { t } = useTranslation();
+
   return (
     <nav className="hidden lg:flex items-center gap-x-4 font-semibold text-xl mt-1.5 dark:text-white">
       {Navs.map((nav, index) => {
@@ -10,7 +13,7 @@ export default function NavBar() {
             href={nav.href}
             className="group lg:hover:text-secondary transition-all duration-200 ease-linear"
           >
-            {nav.name}
+            {t(`navigation.${nav.name}`)}
             <span className="block relative left-1/2 max-w-0 group-hover:max-w-full transition-all duration-500 h-[2.5px] bg-secondary transform -translate-x-1/2"></span>
           </a>
         );
