5b5598a6dfac90b98ec2f6d501f62dbb957a95c2
Peter Pham <pttp15122002@gmail.com>
2024-11-29 19:07:24 +0700

    test: for logo component

---
+++ src/components/Layout/Logos/FullLogo.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import { it, describe, expect } from "vitest";
+import FullLogo from "./FullLogo";
+
+describe("Logo Test", () => {
+  it("should render Logo correctly", () => {
+    const Logo = render(<FullLogo />);
+
+    const LogoText = Logo.getByText("Kaohut!");
+    expect(LogoText).toBeInTheDocument();
+
+    const LogoImage = Logo.getByAltText("Kaohut Full Logo");
+    expect(LogoImage).toBeInTheDocument();
+  });
+});
