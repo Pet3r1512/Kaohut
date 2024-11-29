ab773b9b7f25672b8586a369012471f55f2f9d24
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-11-29 07:58:17 +0700

    #FEAT-014: Header (#15)
    * install shadcn dropdown menu
    
    * chore: un-comment blocks
    
    * feat: add sections to header
    
    * feat: languages toggle component
    
    * feat: settings contains theme and languages toggles
    
    * feat: dropdown menu from shadcn
    
    * feat: use same name with translation keys
    
    * chore: remove unused
    
    * refactor: add code for easy toggling languages
    
    * feat: change lang onClick event
    
    * imp: use translations on navbar's items

---
+++ src/components/Layout/Header/LanguagesToggle.tsx
@@ -0,0 +1,36 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, Languages } from "lucide-react";
+import { useTranslation } from "react-i18next";
+
+export function LanguagesToggle() {
+  const { i18n } = useTranslation();
+
+  // Define language options with their respective codes
+  const Langs = [
+    { name: "Tieng Viet", code: "vi" },
+    { name: "English", code: "en" },
+  ];
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="dark:text-white text-black flex items-center">
+        <Languages />
+        <ChevronDown size={12} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-max flex flex-col gap-y-5 border-none p-5">
+        {Langs.map((lang, index) => (
+          <p
+            key={index}
+            className="lg:hover:text-secondary transition-all duration-150 ease-linear cursor-pointer font-semibold"
+            onClick={() => i18n.changeLanguage(lang.code)} // Change language on click
+          >
+            {lang.name}
+          </p>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,19 +1,21 @@
 import Authentication from "./Authentication";
 import Navbar from "./Navbar";
+import Settings from "./Settings";
+import Sidebar from "./Sidebar";
 
 export default function Header() {
   return (
     <section className="sticky top-0 z-30 !backdrop-filter !backdrop-blur-xl flex justify-center lg:h-24">
       <header className="p-5 max-w-[100rem] mx-auto flex items-center justify-between fixed w-full">
-        {/* <Sidebar /> */}
+        <Sidebar />
         {/* <FullLogo className="lg:hidden" /> */}
         <div className="lg:flex items-center gap-x-7 hidden">
           {/* <FullLogo /> */}
           <Navbar />
         </div>
         <div className="lg:flex items-center gap-x-3.5 hidden">
-          {/* <Settings />
-          <VerticalLine /> */}
+          <Settings />
+          <VerticalLine />
           <Authentication />
         </div>
       </header>
@@ -21,6 +23,6 @@ export default function Header() {
   );
 }
 
-// function VerticalLine() {
-//   return <div className="w-0.5 h-8 dark:bg-white bg-black" />;
-// }
+function VerticalLine() {
+  return <div className="w-0.5 h-8 dark:bg-white bg-black" />;
+}
