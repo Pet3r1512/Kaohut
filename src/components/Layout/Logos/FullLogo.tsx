105f75ce8f07b482ed133298cf5efd5fac5533e6
Peter Pham <pttp15122002@gmail.com>
2024-11-29 10:08:59 +0700

    feat: Fulllogo with logo and name

---
+++ src/components/Layout/Logos/FullLogo.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+
+export default function FullLogo({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn("flex items-center gap-x-2.5 cursor-default", className)}
+    >
+      <img
+        src={"/logos/Logo.png"}
+        alt="Kaohut Full Logo"
+        className="size-8 lg:size-12"
+      />
+      <p className="bg-gradient-to-r from-primary to-secondary inline-block text-transparent bg-clip-text font-bold lg:text-3xl">
+        Kaohut!
+      </p>
+    </div>
+  );
+}
