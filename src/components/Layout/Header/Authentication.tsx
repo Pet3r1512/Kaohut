ad66080d35d76b6dee4410e10a721358d6e12ade
Peter Pham <pttp15122002@gmail.com>
2024-11-29 07:33:04 +0700

    feat: auth section contains sign in and sign up buttons

---
+++ src/components/Layout/Header/Authentication.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+
+export default function Authentication() {
+  const auths = [
+    {
+      name: "Log In",
+      href: "/auth/signin",
+      className: "",
+    },
+    {
+      name: "Sign Up",
+      href: "/auth/signup",
+      className: "!bg-primary text-white",
+    },
+  ];
+  return (
+    <div className="flex items-center gap-x-5">
+      {auths.map((auth, index) => {
+        return (
+          <a
+            key={index}
+            href={auth.href}
+            className={cn(
+              "px-4 pb-3 pt-2.5 rounded-xl shadow-2xl font-semibold bg-white dark:bg-black dark:text-white lg:hover:scale-105 transition-all duration-150 ease-linear",
+              auth.className,
+            )}
+          >
+            {auth.name}
+          </a>
+        );
+      })}
+    </div>
+  );
+}
