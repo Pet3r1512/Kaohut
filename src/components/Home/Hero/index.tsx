1b1c7ce3f97917502429bd9712781cd953d291d1
Linh Dan <118578840+nguyenholinhdan@users.noreply.github.com>
2024-12-02 16:01:02 +0700

    #FEAT-019: hero translations (#27)
    * chore: english translation
    
    * chore: Vietnamese translations
    
    * feat: use translation for buttons
    
    * feat: use translations for hero's title
    
    * feat: use translations for hero's subtitle
    
    * fix: misspelling

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -1,14 +1,16 @@
 import HighlightedTitle from "./HighlightedTitle";
 import Actions from "./Actions";
+import { useTranslation } from "react-i18next";
 
 export default function Hero() {
+  const { t } = useTranslation();
+
   return (
     <section className="min-h-[calc(100dvh-4rem)] lg:min-h-[calc(100dvh-6rem)] flex flex-col justify-center gap-y-7 lg:flex-row w-full md:items-center py-12 lg:py-0 mt-5 lg:mt-0">
       <div className="w-full md:w-9/12 lg:w-7/12 flex flex-col gap-y-5 items-center lg:items-start">
         <HighlightedTitle />
         <p className="md:text-lg lg:text-xl text-center lg:text-left">
-          Transform the way you connect and learn through interactive quizzes,
-          polls, and games—all in one dynamic platform.
+          {t("hero.subtitle")}
         </p>
         <Actions />
       </div>
