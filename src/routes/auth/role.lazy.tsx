c841dd1bbb7428f46f23ffbf18e79d15976b1dfd
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-07 15:14:07 +0700

    #IMP-001: responsive role picker page (#30)
    * style: add animations
    
    * style: increase title font size
    
    * style: responsive
    
    * style: responsive for mobiles
    
    * feat: add logo for navigating homepage
    
    * style: responsive for tablets
    
    * refactor: split to component
    
    * imp: use component instead
    
    * update config for storybook testing
    
    * install dependencies
    
    * test: Role card by storybook
    
    * test: Role card to be in the DOM and match its snapshot
    
    * refactor: add data-testid
    
    * chore: roleCard's snapshot
    
    * imp: delete deprecated packages
    
    * chore: delete deprecated packages
    
    * chore install packages
    
    * style: add max width for role card
    
    * test: update role card's snapshot

--- src/routes/auth/role.lazy.tsx
+++ src/routes/auth/role.lazy.tsx
@@ -1,9 +1,9 @@
-import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
+import RoleCard, { RoleCardProps } from "@/components/Auth/Role/RoleCard";
+import FullLogo from "@/components/Layout/Logos/FullLogo";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { GraduationCap, User } from "lucide-react";
 
-const accountTypes = [
+const accountTypes: RoleCardProps[] = [
   {
     id: "teacher",
     title: "Teacher",
@@ -28,36 +28,18 @@ export const Route = createLazyFileRoute("/auth/role")({
 
 function Role() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="relative flex min-h-[100dvh] items-center justify-center bg-gray-100">
+      <FullLogo className="absolute md:top-10 top-6 md:left-10 left-6" />
       <div className="space-y-6">
-        <h1 className="text-center text-lg md:text-xl lg:text-2xl font-bold">
+        <h1 className="text-center text-xl md:text-2xl lg:text-3xl font-bold">
           Choose your account type
         </h1>
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
           {accountTypes.map(
             ({ id, title, color, bgColor, titleHoverBg, icon }) => (
-              <Card
-                key={id}
-                className="cursor-pointer lg:hover:scale-105 transition-all duration-150 ease-linear group"
-              >
-                <CardHeader
-                  className={`flex justify-center items-center ${color} p-4 rounded-t-lg lg:size-40`}
-                >
-                  <div
-                    className={cn(
-                      "rounded-full lg:group-hover:p-10 transition-all duration-150 ease-linear lg:p-6 text-white",
-                      bgColor,
-                    )}
-                  >
-                    {icon}
-                  </div>
-                </CardHeader>
-                <CardContent
-                  className={`text-center p-4 ${titleHoverBg} rounded-b-lg lg:group-hover:text-white transition-all duration-150 ease-linear`}
-                >
-                  <CardTitle>{title}</CardTitle>
-                </CardContent>
-              </Card>
+              <RoleCard
+                props={{ id, title, color, bgColor, titleHoverBg, icon }}
+              />
             ),
           )}
         </div>
