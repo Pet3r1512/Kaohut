484a062f035bf263063dd9372666a2be80ab11e4
Peter Pham <pttp15122002@gmail.com>
2024-12-07 14:55:26 +0700

    test: Role card to be in the DOM and match its snapshot

---
+++ src/components/Auth/Role/RoleCard.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import RoleCard from "./RoleCard";
+import { GraduationCap } from "lucide-react";
+
+const mockData = {
+  id: "teacher",
+  title: "Teacher",
+  color: "bg-red-500",
+  bgColor: "bg-[#AB0018]",
+  titleHoverBg: "lg:group-hover:bg-[#AB0018]",
+  icon: <GraduationCap className="lg:size-10" />,
+};
+
+it("rendered correctly in DOM", async () => {
+  render(<RoleCard props={mockData} />);
+
+  const RoleCardComponent = screen.getByTestId("card");
+
+  expect(RoleCardComponent).toBeInTheDocument();
+});
+
+it("should match snapshot", async () => {
+  const component = render(<RoleCard props={mockData} />);
+
+  expect(component).toMatchSnapshot();
+});
