20661e431654f68b169dd6ebef80c837c99bb26d
Linh Dan <118578840+nguyenholinhdan@users.noreply.github.com>
2024-12-10 09:33:19 +0700

    #FEAT-021: teacher workplace picker (#32)
    * auto generated
    
    * add href
    
    * add href to teacher page
    
    * build teacher page
    
    * update color
    
    * update title
    
    * update size
    
    * change icon school
    
    * changee icon business
    
    * update icon
    
    * add missing href
    
    * fixbug
    
    * update snapshots

--- src/components/Auth/Role/RoleCard.tsx
+++ src/components/Auth/Role/RoleCard.tsx
@@ -9,34 +9,37 @@ export type RoleCardProps = {
   bgColor: string;
   titleHoverBg: string;
   icon: ReactNode;
+  href: string;
 };
 
 export default function RoleCard({ props }: { props: RoleCardProps }) {
-  const { id, title, bgColor, color, icon, titleHoverBg } = props;
+  const { id, title, bgColor, color, icon, titleHoverBg, href } = props;
   return (
-    <Card
-      data-testid="card"
-      key={id}
-      className="cursor-pointer lg:hover:scale-105 transition-all duration-150 ease-linear group flex flex-row md:flex-col max-w-56"
-    >
-      <CardHeader
-        data-testid="card-header"
-        className={`w-1/3 md:w-full flex justify-center items-center ${color} lg:group-hover:p-0 p-4 lg:rounded-t-lg md:h-36 lg:h-48 transition-all duration-150 ease-linear`}
+    <a href={href}>
+      <Card
+        data-testid="card"
+        key={id}
+        className="cursor-pointer lg:hover:scale-105 transition-all duration-150 ease-linear group flex flex-row md:flex-col max-w-80 lg:w-60"
       >
-        <div
-          className={cn(
-            "rounded-full lg:group-hover:rounded-none lg:group-hover:rounded-t-lg lg:group-hover:size-full lg:group-hover:p-10 transition-all duration-150 ease-linear p-2 md:p-3.5 lg:p-6 text-white flex items-center justify-center",
-            bgColor,
-          )}
+        <CardHeader
+          data-testid="card-header"
+          className={`w-1/3 md:w-full flex justify-center items-center ${color} lg:group-hover:p-0 p-4 lg:rounded-t-lg md:h-36 lg:h-48 transition-all duration-150 ease-linear`}
         >
-          {icon}
-        </div>
-      </CardHeader>
-      <CardContent
-        className={`text-center p-4 ${titleHoverBg} rounded-b-lg lg:group-hover:text-white transition-all duration-150 ease-linear flex items-center justify-center`}
-      >
-        <CardTitle>{title}</CardTitle>
-      </CardContent>
-    </Card>
+          <div
+            className={cn(
+              "rounded-full lg:group-hover:rounded-none lg:group-hover:rounded-t-lg lg:group-hover:size-full lg:group-hover:p-10 transition-all duration-150 ease-linear p-2 md:p-3.5 lg:p-6 text-white flex items-center justify-center",
+              bgColor,
+            )}
+          >
+            {icon}
+          </div>
+        </CardHeader>
+        <CardContent
+          className={`text-center p-4 ${titleHoverBg} rounded-b-lg lg:group-hover:text-white transition-all duration-150 ease-linear flex items-center justify-center`}
+        >
+          <CardTitle>{title}</CardTitle>
+        </CardContent>
+      </Card>
+    </a>
   );
 }
---
+++ src/routes/auth/teacher.lazy.tsx
@@ -0,0 +1,60 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import RoleCard, { RoleCardProps } from "@/components/Auth/Role/RoleCard";
+import FullLogo from "@/components/Layout/Logos/FullLogo";
+import { BookOpen, BriefcaseBusiness, School } from "lucide-react";
+
+const accountTypes: RoleCardProps[] = [
+  {
+    id: "schools",
+    title: "Schools",
+    color: "bg-[#669bbc]",
+    bgColor: "bg-[#003049]",
+    titleHoverBg: "lg:group-hover:bg-[#003049]",
+    icon: <BookOpen className="lg:size-10" />,
+    href: "/auth/teacher",
+  },
+  {
+    id: "university",
+    title: "University",
+    color: "bg-[#669bbc]",
+    bgColor: "bg-[#003049]",
+    titleHoverBg: "lg:group-hover:bg-[#003049]",
+    icon: <School className="lg:size-10" />,
+    href: "/auth/teacher",
+  },
+  {
+    id: "business",
+    title: "Business",
+    color: "bg-[#669bbc]",
+    bgColor: "bg-[#003049]",
+    titleHoverBg: "lg:group-hover:bg-[#003049]",
+    icon: <BriefcaseBusiness className="lg:size-10" />,
+    href: "/auth/teacher",
+  },
+];
+
+export const Route = createLazyFileRoute("/auth/teacher")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="relative flex min-h-[100dvh] items-center justify-center bg-gray-100">
+      <FullLogo className="absolute md:top-10 top-6 md:left-10 left-6" />
+      <div className="flex flex-col gap-y-5">
+        <h1 className="text-center text-xl md:text-2xl lg:text-3xl font-bold">
+          Choose your workplace
+        </h1>
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-3 w-full">
+          {accountTypes.map(
+            ({ id, title, color, bgColor, titleHoverBg, icon, href }) => (
+              <RoleCard
+                props={{ id, title, color, bgColor, titleHoverBg, icon, href }}
+              />
+            ),
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
