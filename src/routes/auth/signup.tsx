8cfaccb37ce5b1ebd30bd7c8ec57ed28fe9683d7
Peter Pham <pttp15122002@gmail.com>
2024-12-10 14:56:31 +0700

    feat: add signup form to signup page

--- src/routes/auth/signup.tsx
+++ src/routes/auth/signup.tsx
@@ -1,3 +1,4 @@
+import { SignupForm } from "@/components/Auth/Forms/Account/SignupForm";
 import AuthLayout from "@/components/Auth/Layout/AuthLayout";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -11,6 +12,7 @@ function RouteComponent() {
       <h1 className="text-center text-xl md:text-2xl lg:text-3xl font-bold">
         Create an account
       </h1>
+      <SignupForm />
     </AuthLayout>
   );
 }
