cdca51a5432426f5551c7338cd8d28a7f902724a
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-10 15:37:15 +0700

    #FEAT-005: signup page (#33)
    * feat: create layout for all auth pages
    
    * ops: routes generated
    
    * feat: go to signup page when complete workplace picker
    
    * feat: signup page (cont)
    
    * install Label component from aceternity
    
    * install Input component from aceternity
    
    * feat: add theme toggle button
    
    * feat: signup form
    
    * feat: add signup form to signup page
    
    * style: responsive
    
    * test: add test-id
    
    * chore: use uppercase letters
    
    * test: signup form's snapshot
    
    * test: signup form  (cont)
    
    * chore: remove unused
    
    * fix: missing packages

---
+++ src/components/Auth/Layout/AuthLayout.tsx
@@ -0,0 +1,15 @@
+import FullLogo from "@/components/Layout/Logos/FullLogo";
+import ThemeToggle from "@/components/ThemeToggle";
+import { ReactNode } from "react";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative flex min-h-[100dvh] items-center justify-center mb-24 lg:mb-0">
+      <div className="absolute md:top-10 top-6 md:left-10 left-6 flex items-center justify-between w-full pr-12 lg:pr-20">
+        <FullLogo />
+        <ThemeToggle />
+      </div>
+      <div className="space-y-6 mt-24 md:mt-12">{children}</div>
+    </div>
+  );
+}
---
+++ src/routes/auth/signup.tsx
@@ -0,0 +1,18 @@
+import { SignupForm } from "@/components/Auth/Forms/Account/SignupForm";
+import AuthLayout from "@/components/Auth/Layout/AuthLayout";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/auth/signup")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <AuthLayout>
+      <h1 className="text-center text-xl md:text-2xl lg:text-3xl font-bold">
+        Create an account
+      </h1>
+      <SignupForm />
+    </AuthLayout>
+  );
+}
