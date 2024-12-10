90bbb752e52f22ca24494e44789204f4c314b874
Peter Pham <pttp15122002@gmail.com>
2024-12-10 15:01:45 +0700

    style: responsive

--- src/components/Auth/Forms/Account/SignupForm.tsx
+++ src/components/Auth/Forms/Account/SignupForm.tsx
@@ -10,7 +10,7 @@ export function SignupForm() {
     console.log("Form submitted");
   };
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="px-5 pt-8 max-w-md w-full mx-auto rounded-lg md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to <span className="text-primary">KaoHut</span>
       </h2>
