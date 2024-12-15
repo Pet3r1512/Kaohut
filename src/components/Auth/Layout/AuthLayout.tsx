76bf446e6af7d4c638a841f87a08ef2fb5e74775
Peter Pham <pttp15122002@gmail.com>
2024-12-15 18:16:39 +0700

    feat: add theme toggle button

--- src/components/Auth/Layout/AuthLayout.tsx
+++ src/components/Auth/Layout/AuthLayout.tsx
@@ -1,3 +1,4 @@
+import { LanguagesToggle } from "@/components/Layout/Header/LanguagesToggle";
 import FullLogo from "@/components/Layout/Logos/FullLogo";
 import ThemeToggle from "@/components/ThemeToggle";
 import { ReactNode } from "react";
@@ -7,7 +8,10 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
     <div className="max-w-screen overflow-x-hidden relative flex min-h-[100dvh] items-center justify-center mb-24 lg:mb-0">
       <div className="absolute md:top-10 top-6 md:left-10 left-6 flex items-center justify-between w-full pr-12 lg:pr-20">
         <FullLogo />
-        <ThemeToggle />
+        <div className="flex items-center gap-x-5">
+          <LanguagesToggle />
+          <ThemeToggle />
+        </div>
       </div>
       <div className="space-y-6 mt-24 md:mt-12">{children}</div>
     </div>
