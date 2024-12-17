2a6ef8ff84c3cb7fa82ae9c1564349cd05272ff5
Peter Pham <pttp15122002@gmail.com>
2024-12-17 08:46:34 +0700

    refactor: use section instead

--- src/components/Auth/Layout/AuthLayout.tsx
+++ src/components/Auth/Layout/AuthLayout.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-screen overflow-x-hidden relative flex min-h-[100dvh] items-center justify-center mb-24 lg:mb-0">
+    <section className="max-w-screen overflow-x-hidden relative flex min-h-[100dvh] items-center justify-center mb-24 lg:mb-0">
       <div className="absolute md:top-10 top-6 md:left-10 left-6 flex items-center justify-between w-full pr-12 lg:pr-20">
         <FullLogo />
         <div className="flex items-center gap-x-5">
@@ -15,7 +15,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
         </div>
       </div>
       <div className="space-y-6 mt-24 md:mt-12">{children}</div>
-    </div>
       <Toaster />
+    </section>
   );
 }
