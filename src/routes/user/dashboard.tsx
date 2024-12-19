53b789043bd2d051c359709d0622a5ed595652b8
Peter Pham <pttp15122002@gmail.com>
2024-12-19 17:36:39 +0700

    debug

--- src/routes/user/dashboard.tsx
+++ src/routes/user/dashboard.tsx
@@ -1,19 +1,25 @@
-import Dashboard from "@/components/User/Dashboard/_index";
 import { authClient } from "@/lib/auth-client";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
+// import { Navigate } from "@tanstack/react-router"; // For redirection
 
 export const Route = createFileRoute("/user/dashboard")({
   component: RouteComponent,
-  beforeLoad: async () => {
-    const { data: session } = await authClient.getSession();
-    if (!session) {
-      throw redirect({
-        to: "/auth/accounts/signin",
-      });
-    }
-  },
 });
 
 function RouteComponent() {
-  return <Dashboard />;
+  const { data, isPending } = authClient.useSession();
+
+  if (isPending) {
+    // While checking session, show a loading state
+    return <div>Loading...</div>;
+  }
+
+  console.log(data);
+  // if (!session) {
+  //   // Redirect to the login page if not authenticated
+  //   return <Navigate to="/auth/accounts/signin" />;
+  // }
+
+  // Render the dashboard if authenticated
+  return <div>Hello "/user/dashboard"!</div>;
 }
