7f3f9a41a71e97e5daa3a529be6281eee66fbd62
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-19 10:49:19 +0700

    #OPS: install better auth client (#61)
    * install better-auth
    
    * fix: missing URL
    
    * ops: config for better-auth at client side
    
    * ops: use better-auth for signup
    
    * ops: use better-auth for signin
    
    * fix: missing URL structure

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:9999"
-    : "https://blonde-michell-pet3r-22028f0a.koyeb.app";
+    ? "http://localhost:9999/api/auth"
+    : "https://blonde-michell-pet3r-22028f0a.koyeb.app/api/auth";
---
+++ src/lib/auth-client.ts
@@ -0,0 +1,20 @@
+import { SERVER_URL } from "@/api/constant";
+import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+export const authClient = createAuthClient({
+  baseURL: SERVER_URL,
+  plugins: [
+    inferAdditionalFields({
+      user: {
+        role: {
+          type: "string",
+          required: true,
+        },
+        workplace: {
+          type: "string",
+          required: true,
+        },
+      },
+    }),
+  ],
+});
