b8bbff5c936fc972090456b3eea38c5491801915
Peter Pham <pttp15122002@gmail.com>
2024-12-19 10:52:13 +0700

    ops: use dynamic callback url instead

--- src/components/Auth/Forms/Account/SigninForm.tsx
+++ src/components/Auth/Forms/Account/SigninForm.tsx
@@ -7,6 +7,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { LoaderCircle } from "lucide-react";
 import { authClient } from "@/lib/auth-client";
 import { toast } from "@/hooks/useToast";
+import { CALLBACK_URL } from "@/api/constant";
 
 type SigninFormInputs = {
   email: string;
@@ -21,7 +22,7 @@ export function SigninForm() {
       {
         email: credentials.email,
         password: credentials.password,
-        callbackURL: "http://localhost:5173/dashboard/dashboard",
+        callbackURL: CALLBACK_URL,
       },
       {
         onRequest: () => {
