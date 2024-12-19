ec05aa7e2198ae633579045d45b752b3fb85ebff
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-19 11:03:29 +0700

    #FIX: invalid callback url (#62)
    * ops: add callback url
    
    * ops: use dynamic callback url instead

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -2,3 +2,8 @@ export const SERVER_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:9999/api/auth"
     : "https://blonde-michell-pet3r-22028f0a.koyeb.app/api/auth";
+
+export const CALLBACK_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5173/dashboard/dashboard"
+    : "https://kaohut.pages.dev/dashboard/dashboard";
