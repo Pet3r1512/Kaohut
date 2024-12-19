aca3330865f46fe1a0862e7abf04a1ab5d543627
Peter Pham <pttp15122002@gmail.com>
2024-12-19 10:36:55 +0700

    fix: missing URL

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:9999"
+    ? "http://localhost:9999/api/auth"
     : "https://blonde-michell-pet3r-22028f0a.koyeb.app";
