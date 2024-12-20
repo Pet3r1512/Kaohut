e5881ae9d721123876f84f92444050cfe76a7f53
Peter Pham <pttp15122002@gmail.com>
2024-12-20 20:12:37 +0700

    ops: check logged in user

--- src/routes/user/dashboard.tsx
+++ src/routes/user/dashboard.tsx
@@ -1,11 +1,12 @@
 import Dashboard from "@/components/User/Dashboard/_index";
 import { authClient } from "@/lib/auth-client";
+// import { authClient } from "@/lib/auth-client";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/user/dashboard")({
   component: RouteComponent,
   beforeLoad: async () => {
-    const { data: session } = await authClient.getSession();
+    const session = authClient.getSession();
     if (!session) {
       throw redirect({
         to: "/auth/accounts/signin",
