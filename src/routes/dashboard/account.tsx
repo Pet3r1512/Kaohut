fe47849c7facf641f50aab976b8bf6d85d748ebb
Peter Pham <pttp15122002@gmail.com>
2024-12-21 13:55:18 +0700

    feat: user account's info page

---
+++ src/routes/dashboard/account.tsx
@@ -0,0 +1,23 @@
+import DashboardLayout from '@/components/User/Dashboard/DashboardLayout'
+import { authClient } from '@/lib/auth-client'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/dashboard/account')({
+  component: RouteComponent,
+  beforeLoad: async () => {
+    const session = authClient.getSession()
+    if (!session) {
+      throw redirect({
+        to: '/auth/accounts/signin',
+      })
+    }
+  },
+})
+
+function RouteComponent() {
+  return (
+    <DashboardLayout>
+      <p>Your Account Detail</p>
+    </DashboardLayout>
+  )
+}
