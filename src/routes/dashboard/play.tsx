bd0e77fa6acf2a1fd82cd542af788695121204a0
Peter Pham <pttp15122002@gmail.com>
2024-12-21 13:55:31 +0700

    feat: play page

---
+++ src/routes/dashboard/play.tsx
@@ -0,0 +1,23 @@
+import DashboardLayout from "@/components/User/Dashboard/DashboardLayout";
+import { authClient } from "@/lib/auth-client";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/dashboard/play")({
+  component: RouteComponent,
+  beforeLoad: async () => {
+    const session = authClient.getSession();
+    if (!session) {
+      throw redirect({
+        to: "/auth/accounts/signin",
+      });
+    }
+  },
+});
+
+function RouteComponent() {
+  return (
+    <DashboardLayout>
+      <p>Let's Play</p>
+    </DashboardLayout>
+  );
+}
