bbafc03185d9f23f7d3d1b48219f4d549e66ef6c
Peter Pham <pttp15122002@gmail.com>
2024-12-23 13:40:06 +0700

    chore: remove unused check

--- src/routes/dashboard/play.tsx
+++ src/routes/dashboard/play.tsx
@@ -1,44 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import TeacherPlay from "@/components/Game/Play/TeacherPlay";
-import LoadingScreen from "@/components/LoadingScreen";
 import DashboardLayout from "@/components/User/Dashboard/DashboardLayout";
-import { authClient } from "@/lib/auth-client";
-import { createFileRoute, useRouter } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/play")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [session, setSession] = useState<any>(null);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const fetchSession = async () => {
-      try {
-        const result = await authClient.getSession();
-        setSession(result.data?.session);
-      } catch (error: any) {
-        console.error("Error fetching session", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchSession();
-  }, []);
-
-  if (loading) {
-    return <LoadingScreen />;
-  }
-
-  if (!session) {
-    router.navigate({ to: "/auth/accounts/signin" });
-    return null;
-  }
-
   return (
     <DashboardLayout>
       <TeacherPlay />
