33a36c2ce1f534be8654bd3c91972843ce48d1ce
Peter Pham <pttp15122002@gmail.com>
2024-12-23 13:40:24 +0700

    ops: enable session checking for all dashboard sections

--- src/components/User/Dashboard/DashboardLayout.tsx
+++ src/components/User/Dashboard/DashboardLayout.tsx
@@ -1,11 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./Sidebar/_index";
+import { useEffect, useState } from "react";
+import { useRouter } from "@tanstack/react-router";
+import LoadingScreen from "@/components/LoadingScreen";
+import { authClient } from "@/lib/auth-client";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [session, setSession] = useState<any>(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+        const result = await authClient.getSession();
+        setSession(result.data?.session);
+      } catch (error: any) {
+        console.error("Error fetching session", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchSession();
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
+  if (!session) {
+    router.navigate({ to: "/auth/accounts/signin" });
+    return null;
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
