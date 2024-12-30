cc743d0d77f14a3197a7c79ec2b1c4a24431dbab
Peter Pham <pttp15122002@gmail.com>
2024-12-30 16:18:39 +0700

    feat: quiz card info

---
+++ src/components/Game/Quiz/QuizCard.tsx
@@ -0,0 +1,17 @@
+export type Quiz = {
+  id: string;
+  title: string;
+  description: string;
+  creatorId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export default function QuizCard({ quiz }: { quiz: Quiz }) {
+  return (
+    <div className="bg-white shadow-2xl rounded-2xl p-5 w-96 h-36 lg:scale-95 lg:hover:scale-100 transition-all duration-150 ease-linear">
+      <p className="lg:text-xl font-bold lg:mb-2.5 truncate">{quiz.title}</p>
+      <p>{quiz.description}</p>
+    </div>
+  );
+}
