7c5a52e684b58415e2bd8d84b3450581e19b2b12
Peter Pham <pttp15122002@gmail.com>
2024-12-30 16:18:16 +0700

    ops: query URL for different envs

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -7,3 +7,8 @@ export const CALLBACK_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5173/dashboard/play"
     : "https://kaohut.pages.dev/dashboard/play";
+
+export const QUERY_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:9999/trpc"
+    : "https://blonde-michell-pet3r-22028f0a.koyeb.app/trpc"
