2649f720dde3d4129337e676bc1bda0191cf0e5b
Peter Pham <pttp15122002@gmail.com>
2024-12-30 20:23:04 +0700

    fixbug

--- src/hooks/useCheckSession.ts
+++ src/hooks/useCheckSession.ts
@@ -1,37 +1,44 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { authClient } from "@/lib/auth-client";
 import { useUserStore } from "@/stores/user";
 import { useRouter } from "@tanstack/react-router";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useCheckSession() {
-  const [fetching, setFetching] = useState<boolean>(false);
-  const { setUser } = useUserStore();
-  const router = useRouter();
+    const [fetching, setFetching] = useState<boolean>(false)
+    const { setUser } = useUserStore()
+    const router = useRouter()
 
-  const { isPending, data } = authClient.useSession();
+    useEffect(() => {
+        const fetchSession = async () => {
+            setFetching(true)
+            try {
+                const result = await authClient.getSession();
+                console.log(result)
+                // if (!result.data?.session) {
+                //     router.navigate({
+                //         to: "/auth/accounts/signin"
+                //     })
+                // }
+                // store current user info to zustand store
+                if (result.data?.user) {
+                    const user = result.data?.user
+                    setUser({
+                        name: user.name,
+                        email: user.email,
+                        role: user.role,
+                        workplace: user.workplace
+                    })
+                }
+            } catch (error: any) {
+                console.error("Error fetching session", error);
+            } finally {
+                setFetching(false);
+            }
+        };
+        fetchSession();
+    }, []);
 
-  useEffect(() => {
-    setFetching(true);
-
-    if (!isPending && !data?.user) {
-      console.log(data?.session);
-      // router.navigate({
-      //     to: "/auth/accounts/signin",
-      // });
-    }
-
-    if (data?.user) {
-      const user = data.user;
-      setUser({
-        name: user.name,
-        email: user.email,
-        role: user.role,
-        workplace: user.workplace,
-      });
-    }
-
-    setFetching(false);
-  }, [isPending, data, router, setUser]);
-
-  return { fetching, data };
+    return { fetching }
 }
