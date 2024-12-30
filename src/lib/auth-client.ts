f89bd84c384a7a1e5fae0cec7d4f65a9917c5d25
Peter Pham <pttp15122002@gmail.com>
2024-12-30 16:42:26 +0700

    fix: remove unused

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { SERVER_URL } from "@/api/constant";
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
-import { customSessionClient } from "better-auth/client/plugins";
-import { auth } from "./auth";
 
 export const authClient = createAuthClient({
   baseURL: SERVER_URL,
@@ -19,9 +17,5 @@ export const authClient = createAuthClient({
         },
       },
     }),
-    customSessionClient<typeof auth>(),
   ],
-  advanced: {
-    useSecureCookies: true,
-  },
 });
