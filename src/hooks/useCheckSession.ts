e8bd4678feddd7413abbc3a739a06cc001912d9e
Peter Pham <pttp15122002@gmail.com>
2025-01-02 09:55:09 +0700

    try to use infer

--- src/hooks/useCheckSession.ts
+++ src/hooks/useCheckSession.ts
@@ -6,38 +6,38 @@ import { useRouter } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 
 export function useCheckSession() {
-    const [fetching, setFetching] = useState<boolean>(false)
-    const { setUser } = useUserStore()
-    const router = useRouter()
+  const [fetching, setFetching] = useState<boolean>(false)
+  const { setUser } = useUserStore()
+  const router = useRouter()
 
-    useEffect(() => {
-        const fetchSession = async () => {
-            setFetching(true)
-            try {
-                const result = await authClient.getSession();
-                if (!result.data?.session) {
-                    router.navigate({
-                        to: "/auth/accounts/signin"
-                    })
-                }
-                // store current user info to zustand store
-                if (result.data?.user) {
-                    const user = result.data?.user
-                    setUser({
-                        name: user.name,
-                        email: user.email,
-                        role: user.role,
-                        workplace: user.workplace
-                    })
-                }
-            } catch (error: any) {
-                console.error("Error fetching session", error);
-            } finally {
-                setFetching(false);
-            }
-        };
-        fetchSession();
-    }, []);
+  useEffect(() => {
+    const fetchSession = async () => {
+      setFetching(true)
+      try {
+        const result = authClient.$Infer.Session;
+        if (!result?.session) {
+          router.navigate({
+            to: "/auth/accounts/signin"
+          })
+        }
+        // store current user info to zustand store
+        if (result?.user) {
+          const user = result?.user
+          setUser({
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            workplace: user.workplace
+          })
+        }
+      } catch (error: any) {
+        console.error("Error fetching session", error);
+      } finally {
+        setFetching(false);
+      }
+    };
+    fetchSession();
+  }, []);
 
-    return { fetching }
+  return { fetching }
 }
