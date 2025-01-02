4e824b058cf1890ae32f1d5aa1e47f53a1ffec06
Linh Dan <118578840+nguyenholinhdan@users.noreply.github.com>
2025-01-02 13:57:43 +0700

    #TEST-007: loading screen (#87)
    * debug: session
    
    * fix: remove unused
    
    * #FIX: session does not work in prod (#84)
    
    * fix: invalid session on prod
    
    * fixbug
    
    * fixbugs
    
    * fix bug
    
    * fixbug
    
    * fix: bug
    
    * debug
    
    * try: add import from server
    
    * debug
    
    * fix: bug on prod
    
    * fix: rollback
    
    * Fixbug
    
    * try
    
    * try: signin by button
    
    * fix: eslint warnings
    
    * debug: getSession in prod
    
    * chore: remove unused
    
    * debug: session when signin complete
    
    * try: useSession
    
    * fixbug
    
    * debug
    
    * debug
    
    * test: test for loading screen
    
    * test: add snapshot
    
    * test: add test id
    
    ---------
    
    Co-authored-by: Peter Pham <pttp15122002@gmail.com>
    Co-authored-by: Peter Pham <89619124+Pet3r1512@users.noreply.github.com>

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -2,7 +2,10 @@ import { LoaderCircle } from "lucide-react";
 
 export default function LoadingScreen() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div
+      data-testid="LoadingScreen"
+      className="w-screen h-screen flex items-center justify-center"
+    >
       <LoaderCircle size={48} className="animate-spin text-primary" />
     </div>
   );
