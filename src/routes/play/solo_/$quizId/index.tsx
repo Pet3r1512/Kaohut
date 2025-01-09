14b7314a22c5219e526e7c19a2f51d7f16bd120e
Peter Pham <pttp15122002@gmail.com>
2025-01-09 16:10:19 +0700

    chore: remove carousel

--- src/routes/play/solo_/$quizId/index.tsx
+++ src/routes/play/solo_/$quizId/index.tsx
@@ -1,17 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react-hooks/exhaustive-deps */
 import { GetQuiz } from "@/api/quiz/getQuiz";
 import LoadingScreen from "@/components/LoadingScreen";
 import { useQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { io } from "socket.io-client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import QuestionCard from "@/components/Game/Play/QuestionCard";
+import { LoaderCircle, Play } from "lucide-react";
 
 const socket = io("http://localhost:9999");
 
@@ -22,6 +17,8 @@ export const Route = createFileRoute("/play/solo_/$quizId/")({
 function RouteComponent() {
   const [currentQuestion, setCurrentQuestion] = useState<any>(null);
   const [finalScore, setFinalScore] = useState<number | null>(null);
+  const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
+  const [questionFetching, setQuestionFetching] = useState<boolean>(false);
   const { quizId } = Route.useParams();
 
   const { isPending, data, error } = useQuery({
@@ -30,14 +27,8 @@ function RouteComponent() {
     enabled: !!quizId,
   });
 
-  useEffect(() => {
-    if (data) {
-      const currentUserId = "z_U6sTT1JUdg5Ns7E7Ye9";
-      startSinglePlayer(quizId, currentUserId);
-    }
-  }, [data]);
-
   const startSinglePlayer = (quizId: string, currentUserId: string) => {
+    setQuestionFetching(true);
     socket.emit(
       "start_single_player",
       { quizId, currentUserId },
@@ -49,6 +40,7 @@ function RouteComponent() {
 
         if (response.question) {
           setCurrentQuestion(response.question);
+          setQuestionFetching(false);
         }
       },
     );
@@ -87,33 +79,53 @@ function RouteComponent() {
   if (finalScore !== null) {
     return (
       <div className="flex items-center justify-center h-screen bg-gray-300">
-        <p className="text-black font-bold text-2xl">
-          Quiz Finished! Your Final Score: {finalScore}
-        </p>
+        <div className="lg:w-[450px] lg:h-[750px] rounded-2xl shadow-2xl p-5 bg-gradient-to-bl from-[#654ea3] to-[#eaafc8] flex flex-col items-center justify-center gap-y-5 text-white">
+          <p className="text-4xl font-bold">Quiz Finished!</p>
+          <p className="text-2xl font-bold">Your Final Score: {finalScore}</p>
+          <Link
+            className="bg-blue-600 text-white font-bold px-5 py-2.5 rounded-2xl"
+            to="/dashboard/play"
+          >
+            Go Home
+          </Link>
+        </div>
       </div>
     );
   }
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-300">
-      <Carousel>
-        <CarouselContent className="lg:w-[450px] lg:h-[750px] !rounded-2xl">
-          {currentQuestion ? (
-            <CarouselItem>
-              <QuestionCard
-                question={currentQuestion}
-                onAnswer={answerQuestion}
-              />
-            </CarouselItem>
-          ) : (
-            <CarouselItem className="shadow-2xl p-5 bg-gradient-to-bl from-[#654ea3] to-[#eaafc8] flex flex-col items-center justify-center gap-y-14">
-              <p className="text-white lg:text-4xl font-bold">
-                {data.quiz.result.data.quiz.title}
-              </p>
-            </CarouselItem>
-          )}
-        </CarouselContent>
-      </Carousel>
+      <div className="lg:w-[450px] lg:h-[750px] rounded-2xl shadow-2xl p-5 bg-gradient-to-bl from-[#654ea3] to-[#eaafc8] flex flex-col items-center justify-center gap-y-14">
+        {!isGameStarted ? (
+          <>
+            <p className="text-white lg:text-4xl font-bold">
+              {data.quiz.result.data.quiz.title}
+            </p>
+            <button
+              onClick={() => {
+                startSinglePlayer(quizId, "z_U6sTT1JUdg5Ns7E7Ye9");
+                setIsGameStarted(true);
+              }}
+              className=" text-white font-bold bg-green-600 px-5 py-2 rounded-lg"
+            >
+              {questionFetching ? (
+                <LoaderCircle className="animate-spin mx-auto" />
+              ) : (
+                <p className="flex items-center gap-x-1.5">
+                  Start Now <Play />
+                </p>
+              )}
+            </button>
+          </>
+        ) : (
+          !questionFetching && (
+            <QuestionCard
+              question={currentQuestion}
+              onAnswer={answerQuestion}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 }
