8087bdc22a0c3e3d9d32bb75ec0d0a528e5f01a2
Peter Pham <pttp15122002@gmail.com>
2025-01-13 13:52:18 +0700

    feat: display correct answer

--- src/components/Game/Play/QuestionCard.tsx
+++ src/components/Game/Play/QuestionCard.tsx
@@ -1,17 +1,32 @@
 import { Card } from "@/components/ui/card";
+import { useState } from "react";
 
 interface QuestionCardProps {
   question: {
     questionText: string;
     answers: { answerText: string; isCorrect: boolean }[];
   };
-  onAnswer: (answerIndex: number) => void; // Function to handle answering
+  onAnswer: (answerIndex: number) => void;
+  answerState: string | null;
 }
 
 export default function QuestionCard({
   question,
   onAnswer,
 }: QuestionCardProps) {
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+
+  const handleAnswer = (index: number) => {
+    setSelectedAnswer(index);
+
+    setTimeout(() => {
+      setTimeout(() => {
+        setSelectedAnswer(null);
+        onAnswer(index);
+      }, 1000);
+    }, 2000);
+  };
+
   return (
     <Card className="size-full bg-transparent border-none flex flex-col items-center justify-center gap-y-8">
       <div className="h-1/2 flex items-center justify-center w-full">
@@ -19,15 +34,23 @@ export default function QuestionCard({
           {question.questionText}
         </p>
       </div>
-      <div className="w-full h-1/2 grid grid-cols-2 grid-rows-2 gap-2.5 text-white font-semibold">
+      <div className="w-full h-1/2 grid grid-cols-2 grid-rows-2 gap-5 text-white font-semibold">
         {question.answers.map((answer, index) => (
           <button
             key={index}
-            onClick={() => onAnswer(index)}
-            className={`p-5 rounded-xl text-center w-full text-lg ${
-              ["bg-red-500", "bg-yellow-500", "bg-blue-500", "bg-green-500"][
-                index % 4
-              ]
+            onClick={() => handleAnswer(index)}
+            disabled={selectedAnswer !== null} // Disable buttons while an animation is active
+            className={`p-5 rounded-xl text-center w-full text-xl lg:text-3xl transition-all duration-500 ${
+              selectedAnswer !== null
+                ? answer.isCorrect
+                  ? "bg-green-500 scale-105"
+                  : "bg-red-500 scale-95 opacity-75"
+                : [
+                    "bg-red-500",
+                    "bg-yellow-500",
+                    "bg-blue-500",
+                    "bg-green-500",
+                  ][index % 4]
             }`}
           >
             {answer.answerText}
