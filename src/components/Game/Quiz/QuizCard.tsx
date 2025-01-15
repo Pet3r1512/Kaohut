de2a4c3e88c5ee6c6be8c73ddd8435cd70b4ac8f
Peter Pham <pttp15122002@gmail.com>
2025-01-15 10:22:49 +0700

    style: add height for quiz cards

--- src/components/Game/Quiz/QuizCard.tsx
+++ src/components/Game/Quiz/QuizCard.tsx
@@ -12,12 +12,12 @@ export type Quiz = {
 
 export default function QuizCard({ quiz }: { quiz: Quiz }) {
   return (
-    <div className="bg-white flex flex-col justify-between gap-y-5 rounded-2xl p-3.5 md:w-1/2 md:max-w-96 lg:w-96 lg:hover:scale-105 transition-all duration-150 ease-linear">
+    <div className="bg-white flex flex-col justify-between gap-y-5 rounded-2xl p-3.5 md:w-1/2 md:max-w-96 lg:w-96 h-40 lg:h-48 lg:hover:scale-105 transition-all duration-150 ease-linear">
       <div className="flex items-center justify-between">
         <p className="lg:text-xl font-bold lg:mb-2.5 truncate">{quiz.title}</p>
         <p className="text-black text-sm">{quiz.category}</p>
       </div>
-      <p>{quiz.description}</p>
+      <p className="h-[calc(2*2.25rem)] line-clamp-2">{quiz.description}</p>
       <Link
         to={`/play/solo/${quiz.id}`}
         className="bg-green-500 px-2.5 py-1 rounded-lg text-white font-semibold w-fit ml-auto"
