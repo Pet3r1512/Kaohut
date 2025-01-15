f7690eb87813380fbd55adeb19e13767ec2216a7
Peter Pham <pttp15122002@gmail.com>
2025-01-15 09:46:22 +0700

    feat: add question order and custom time

--- src/components/Game/Play/QuestionCard.tsx
+++ src/components/Game/Play/QuestionCard.tsx
@@ -9,11 +9,18 @@ interface QuestionCardProps {
   };
   onAnswer: (answerIndex: number) => void;
   answerState: string | null;
+  questionOrder: {
+    current: number;
+    total: number;
+  };
+  duration: number;
 }
 
 export default function QuestionCard({
   question,
   onAnswer,
+  questionOrder,
+  duration,
 }: QuestionCardProps) {
   const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
 
@@ -42,14 +49,20 @@ export default function QuestionCard({
 
   return (
     <Card className="size-full bg-transparent border-none flex flex-col items-center justify-center gap-y-8 relative">
-      <div className="absolute top-0 right-0">
-        <TimerCircle duration={20} onComplete={handleTimerComplete} />
+      <div className="absolute top-0 left-0 bg-yellow-200 text-black font-bold rounded-full py-2 px-4">
+        Question {questionOrder.current}/{questionOrder.total}
       </div>
+
+      <div className="absolute top-0 right-0">
+        <TimerCircle duration={duration} onComplete={handleTimerComplete} />
+      </div>
+
       <div className="h-1/2 flex flex-col items-center justify-center w-full">
         <p className="text-black font-bold text-xl text-center bg-yellow-200 rounded-xl p-5 w-full">
           {question.questionText}
         </p>
       </div>
+
       <div className="w-full h-1/2 grid grid-cols-2 grid-rows-2 gap-5 text-white font-semibold">
         {question.answers.map((answer, index) => (
           <button
