f109b2cc497b09c654fe522ef1f40cd801289f75
Peter Pham <pttp15122002@gmail.com>
2025-01-18 11:34:53 +0700

    style: add color for dark mode

--- src/components/Game/Quiz/QuizCard.tsx
+++ src/components/Game/Quiz/QuizCard.tsx
@@ -12,10 +12,10 @@ export type Quiz = {
 
 export default function QuizCard({ quiz }: { quiz: Quiz }) {
   return (
-    <div className="bg-white flex flex-col justify-between gap-y-5 rounded-2xl p-3.5 md:w-1/2 w-full md:max-w-96 lg:w-96 h-48 lg:hover:scale-105 transition-all duration-150 ease-linear">
+    <div className="bg-white dark:bg-[#212529] dark:text-white flex flex-col justify-between gap-y-5 rounded-2xl p-3.5 md:w-1/2 w-full md:max-w-96 lg:w-96 h-48 lg:hover:scale-105 transition-all duration-150 ease-linear">
       <div className="flex items-center justify-between">
         <p className="lg:text-xl font-bold lg:mb-2.5 truncate">{quiz.title}</p>
-        <p className="text-black text-sm">{quiz.category}</p>
+        <p className="text-black dark:text-white text-sm">{quiz.category}</p>
       </div>
       <p className="lg:h-[calc(2*2.25rem)] h-[calc(2*3.25rem)] line-clamp-2">
         {quiz.description}
