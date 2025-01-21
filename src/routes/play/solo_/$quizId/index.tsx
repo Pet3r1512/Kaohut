dd3f4866b9920c284df268ae47a10f482adf4dcf
Peter Pham <pttp15122002@gmail.com>
2025-01-21 13:44:28 +0700

    fix(style): remove black background color

--- src/routes/play/solo_/$quizId/index.tsx
+++ src/routes/play/solo_/$quizId/index.tsx
@@ -111,7 +111,7 @@ function RouteComponent() {
 
   return (
     <div className="flex items-center justify-center h-[100dvh] bg-gray-300">
-      <div className="w-full h-[100dvh] lg:w-[450px] lg:h-[750px] lg:rounded-2xl shadow-2xl p-5 bg-gradient-to-bl from-[#654ea3] to-[#eaafc8] flex flex-col items-center justify-center gap-y-14">
+      <div className="w-full h-[100dvh] lg:w-[450px] lg:h-[750px] lg:rounded-2xl shadow-2xl p-5 !bg-gradient-to-bl from-[#654ea3] to-[#eaafc8] flex flex-col items-center justify-center gap-y-14">
         {!isGameStarted ? (
           <>
             <p className="text-white text-3xl lg:text-4xl font-bold">
