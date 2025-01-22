052ecf53c51e1fe95ce37ae9dc2d225e3fb78190
Peter Pham <pttp15122002@gmail.com>
2025-01-22 14:16:51 +0700

    feat: pre-fetch user info

--- src/components/User/Dashboard/DashboardLayout.tsx
+++ src/components/User/Dashboard/DashboardLayout.tsx
@@ -1,7 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./Sidebar/_index";
 import LoadingScreen from "@/components/LoadingScreen";
 import { useCheckSession } from "@/hooks/useCheckSession";
+import { useMutation } from "@tanstack/react-query";
+import { getUserByEmail } from "@/api/user/getUser";
+import { useUserStore, defaultUser } from "@/stores/user";
+import Cookies from "universal-cookie";
+import { useEffect } from "react";
 
 export default function DashboardLayout({
   children,
@@ -9,6 +15,34 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   const { fetching } = useCheckSession();
+  const { getUser, setUser } = useUserStore();
+
+  const mutation = useMutation({
+    mutationFn: getUserByEmail,
+    mutationKey: ["user"],
+    onSuccess: (data) => {
+      const user = data.user.user;
+      setUser({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        workplace: user.workplace,
+      });
+    },
+    onError: (error) => {
+      console.error("Error fetching user:", error.message);
+    },
+  });
+
+  useEffect(() => {
+    const cookies = new Cookies(null, { path: "/" });
+    const user = getUser();
+    if (JSON.stringify(user) === JSON.stringify(defaultUser)) {
+      mutation.mutate(cookies.get("userEmail"));
+      setUser(mutation.data?.user.user);
+    }
+  }, []);
 
   if (fetching) {
     return <LoadingScreen />;
