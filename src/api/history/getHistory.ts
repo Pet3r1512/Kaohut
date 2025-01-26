2c9aaa89415c7520e0fe3a89f1f79823fcbd25c0
Peter Pham <pttp15122002@gmail.com>
2025-01-26 11:59:36 +0700

    ops: api for getting curr user's history

---
+++ src/api/history/getHistory.ts
@@ -0,0 +1,21 @@
+import { QUERY_URL } from "../constant";
+
+export async function getHistory(userId: string) {
+    const response = await fetch(`${QUERY_URL}/history.getHistory`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ userId })
+    })
+
+    if (!response.ok) {
+        const res = await response.json()
+        throw new Error(res.error || "Failed to fetch history")
+    }
+
+    const res = await response.json()
+    return {
+        history: res.result.data.histories
+    }
+}
