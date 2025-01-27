2b4706401519025a131b5de1305cc1ae42335758
Peter Pham <pttp15122002@gmail.com>
2025-01-27 18:08:03 +0700

    feat: display days duration

--- src/components/User/Dashboard/Library/History/HistoryCard.tsx
+++ src/components/User/Dashboard/Library/History/HistoryCard.tsx
@@ -1,4 +1,6 @@
 import { Card } from "@/components/ui/card";
+import CalculateDays from "@/utils/calculateDays";
+import PerformanceDispaly from "@/utils/PerformanceDisplay";
 
 export interface HistoryCardProps {
   id: string;
@@ -6,6 +8,7 @@ export interface HistoryCardProps {
   quizId: string;
   quizName: string;
   score: number;
+  performance: string;
   playedAt: Date;
 }
 
@@ -14,20 +17,38 @@ export default function HistoryCard({
 }: {
   history: HistoryCardProps;
 }) {
-  const { quizName, score, playedAt } = history;
+  const { quizName, score, playedAt, performance } = history;
+  const played = CalculateDays(playedAt);
+
+  const playedPattern: Record<number, string> = {
+    0: "Today",
+    1: "Yesterday",
+  };
+  let playedString = playedPattern[played] || `${played} days ago`;
+
+  if (played > 30) {
+    const months = Math.floor(played / 30);
+    playedString = `${months} month${months > 1 ? "s" : ""} ago`;
+  }
+
   return (
     <Card className="flex justify-between bg-white dark:bg-black shadow-2xl rounded-2xl p-5 lg:hover:scale-105 transition-all duration-150 ease-linear">
-      <p className="text-lg md:text-xl lg:text-2xl font-bold text-secondary">
-        {quizName}
-      </p>
+      <div className="flex flex-col gap-y-2.5">
+        <p className="text-lg md:text-xl lg:text-2xl font-bold text-secondary">
+          {quizName}
+        </p>
+        <PerformanceDispaly perf={performance} />
+      </div>
       <div>
         <p>
           🏆 <span className="text-lg font-semibold">Score</span>:{" "}
           <span className="text-green-500 text-xl font-bold">{score}</span>
         </p>
         <p>
-          ⏰ <span className="text-lg font-semibold">Time</span>:{" "}
-          {new Date(playedAt).toLocaleDateString("en-GB")}
+          ⏰ <span className="text-lg font-semibold">Played</span>:{" "}
+          <span className="text-[#f15bb5] font-bold text-xl">
+            {playedString}
+          </span>
         </p>
       </div>
     </Card>
