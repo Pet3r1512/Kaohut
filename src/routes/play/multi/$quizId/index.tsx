c414217c761663ab0f1006f4bc453153333067d0
Peter Pham <pttp15122002@gmail.com>
2025-01-30 08:47:57 +0700

    style: responsive for mobiles

--- src/routes/play/multi/$quizId/index.tsx
+++ src/routes/play/multi/$quizId/index.tsx
@@ -29,9 +29,11 @@ function RouteComponent() {
 
   return (
     <section className="p-5 flex flex-col h-screen w-screen bg-gradient-to-br from-[#d9a7c7] via-[#e1eec3] to-[#fffcdc] justify-center items-center text-white">
-      <div className="flex flex-col justify-center size-4/5 rounded-2xl shadow-2xl bg-[#669bbc] p-5 lg:p-10 gap-y-5 lg:gap-y-10">
+      <div className="flex flex-col justify-center md:size-4/5 rounded-2xl shadow-2xl bg-[#669bbc] p-5 lg:p-10 gap-y-5 lg:gap-y-10">
         <div className="space-y-5">
-          <p className="lg:text-7xl font-bold italic">Multiplayer Mode</p>
+          <p className="text-3xl md:text-5xl lg:text-7xl font-bold italic">
+            Multiplayer Mode
+          </p>
           <div className="flex gap-x-2.5">
             <CategoryBadge category={data?.quiz.result.data.quiz.category} />
             <TimeBadge time={data?.quiz.result.data.quiz.time} />
@@ -41,14 +43,14 @@ function RouteComponent() {
         <p className="lg:text-xl font-semibold">
           {data?.quiz.result.data.quiz.description}
         </p>
-        <div className="flex flex-col bg-white text-primary p-3.5 rounded-xl">
+        <div className="flex flex-col bg-white text-primary p-2 md:p-3 lg:p-3.5 rounded-xl">
           <div className="flex items-center justify-between">
             <p className="lg:text-3xl font-extrabold">
               {data?.quiz.result.data.quiz.title}
             </p>
             <Link
               to={`/play/multi/${quizId}/waiting`}
-              className="lg:text-xl font-bold bg-secondary px-3.5 py-2 rounded-xl text-white lg:hover:bg-secondary/80 transition-all duration-150 ease-linear"
+              className="lg:text-xl font-bold bg-secondary lg:px-3.5 px-2.5 py-1.5 lg:py-2 rounded-xl text-white lg:hover:bg-secondary/80 transition-all duration-150 ease-linear"
             >
               Create Room
             </Link>
