52bde37f7eccd007615106df596171a60eb17da2
Peter Pham <pttp15122002@gmail.com>
2025-01-30 08:43:32 +0700

    feat: display badges and quiz's info

--- src/routes/play/multi/$quizId/index.tsx
+++ src/routes/play/multi/$quizId/index.tsx
@@ -1,3 +1,9 @@
+import { GetQuiz } from "@/api/quiz/getQuiz";
+import CategoryBadge from "@/components/Game/Multiplayer/Badges/CategoryBadge";
+import LengthBadge from "@/components/Game/Multiplayer/Badges/LengthBadge";
+import TimeBadge from "@/components/Game/Multiplayer/Badges/TimeBadge";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/play/multi/$quizId/")({
@@ -20,19 +26,26 @@ function RouteComponent() {
   if (error) {
     console.error(error.message);
   }
+
   return (
     <section className="p-5 flex flex-col h-screen w-screen bg-gradient-to-br from-[#d9a7c7] via-[#e1eec3] to-[#fffcdc] justify-center items-center text-white">
       <div className="flex flex-col justify-center size-4/5 rounded-2xl shadow-2xl bg-[#669bbc] p-5 lg:p-10 gap-y-5 lg:gap-y-10">
-        <p className="lg:text-7xl font-bold italic">Multiplayer Mode</p>
+        <div className="space-y-5">
+          <p className="lg:text-7xl font-bold italic">Multiplayer Mode</p>
+          <div className="flex gap-x-2.5">
+            <CategoryBadge category={data?.quiz.result.data.quiz.category} />
+            <TimeBadge time={data?.quiz.result.data.quiz.time} />
+            <LengthBadge length={data?.quiz.result.data.quiz.length} />
+          </div>
+        </div>
         <p className="lg:text-xl font-semibold">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione vel
-          corrupti aperiam magnam, quisquam cumque id alias natus possimus
-          perferendis voluptate repellat perspiciatis? Sunt esse distinctio
-          exercitationem beatae dolorum accusantium.
+          {data?.quiz.result.data.quiz.description}
         </p>
         <div className="flex flex-col bg-white text-primary p-3.5 rounded-xl">
           <div className="flex items-center justify-between">
-            <p className="lg:text-3xl font-extrabold">This is Quiz Name</p>
+            <p className="lg:text-3xl font-extrabold">
+              {data?.quiz.result.data.quiz.title}
+            </p>
             <Link
               to={`/play/multi/${quizId}/waiting`}
               className="lg:text-xl font-bold bg-secondary px-3.5 py-2 rounded-xl text-white lg:hover:bg-secondary/80 transition-all duration-150 ease-linear"
@@ -41,6 +54,12 @@ function RouteComponent() {
             </Link>
           </div>
         </div>
+        <p className="lg:text-2xl font-bold">
+          Creator:{" "}
+          <span className="font-semibold text-">
+            {data?.quiz.result.data.quiz.creatorName}
+          </span>
+        </p>
       </div>
     </section>
   );
