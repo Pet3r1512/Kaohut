21e44c96072b90e3bda4da3a157130b5e939fa0e
Peter Pham <pttp15122002@gmail.com>
2025-01-31 13:55:44 +0700

    fix: wrong regex

--- src/components/Auth/Forms/Account/SignupForm.tsx
+++ src/components/Auth/Forms/Account/SignupForm.tsx
@@ -150,7 +150,7 @@ export function SignupForm() {
                 message: t("auth.signup-page.password.condition"),
               },
               pattern: {
-                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z\d]).{8,}$/,
                 message: t("auth.signup-page.password.note"),
               },
             })}
@@ -174,7 +174,7 @@ export function SignupForm() {
                 message: "Password must be at least 8 characters long",
               },
               pattern: {
-                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z\d]).{8,}$/,
                 message:
                   "Password must have at least 1 number, 1 uppercase, 1 lowercase, and 1 special character",
               },
