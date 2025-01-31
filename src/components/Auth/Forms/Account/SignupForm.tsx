b6d90f578534b393f6dba4fc4b49471ac6495a70
Peter Pham <pttp15122002@gmail.com>
2025-01-31 13:50:20 +0700

    fix: regex for confirm password input

--- src/components/Auth/Forms/Account/SignupForm.tsx
+++ src/components/Auth/Forms/Account/SignupForm.tsx
@@ -174,8 +174,7 @@ export function SignupForm() {
                 message: "Password must be at least 8 characters long",
               },
               pattern: {
-                value:
-                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d@$!%*?&#]{8,}$/,
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
                 message:
                   "Password must have at least 1 number, 1 uppercase, 1 lowercase, and 1 special character",
               },
