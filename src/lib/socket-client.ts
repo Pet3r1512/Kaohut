f3965defac3adf13d71973b9912595048bbef3e0
Peter Pham <pttp15122002@gmail.com>
2025-02-04 14:51:26 +0700

    refactor: remove unsused

--- src/lib/socket-client.ts
+++ src/lib/socket-client.ts
@@ -1,8 +1,11 @@
 import { io, Socket } from "socket.io-client";
 
+// Declare socket globally, but do not initialize it until connectSocket is called.
 let socket: Socket | null = null;
 
-export const SOCKET_URL = import.meta.env.MODE === "development" ? "ws://localhost:9999" : "wss://blonde-michell-pet3r-22028f0a.koyeb.app"
+export const SOCKET_URL = import.meta.env.MODE === "development"
+    ? "ws://localhost:9999"
+    : "wss://blonde-michell-pet3r-22028f0a.koyeb.app";
 
 export const connectSocket = () => {
     if (!socket) {
@@ -19,8 +22,5 @@ export const disconnectSocket = () => {
 };
 
 export const getSocket = () => {
-    if (!socket) {
-        throw new Error("Socket connection has not been established. Call connectSocket first.");
-    }
     return socket;
 };
