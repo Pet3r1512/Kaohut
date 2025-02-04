3cd3c355ac70dbc8542bed2e9cd921476cb4e82a
Peter Pham <pttp15122002@gmail.com>
2025-02-04 14:51:08 +0700

    refactor

--- src/routes/play/multi/$quizId/waiting.tsx
+++ src/routes/play/multi/$quizId/waiting.tsx
@@ -1,20 +1,10 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import Lobby from "@/components/Game/Lobby/_index";
-import { SOCKET_URL } from "@/lib/socket-client";
+import useQuizGameSocket from "@/hooks/useSelectQuizAndCreateGame";
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-
-const socket = io(SOCKET_URL);
-
-type MultiplayerRoomSearchParams = {
-  hostname: string;
-};
 
 export const Route = createFileRoute("/play/multi/$quizId/waiting")({
   component: RouteComponent,
-  validateSearch: (search): MultiplayerRoomSearchParams => {
+  validateSearch: (search) => {
     return {
       hostname: String(search.hostname),
     };
@@ -22,27 +12,9 @@ export const Route = createFileRoute("/play/multi/$quizId/waiting")({
 });
 
 function RouteComponent() {
-  const [gameCode, setGameCode] = useState<string>("");
   const { quizId } = Route.useParams();
   const { hostname } = Route.useSearch();
+  const { gameCode, players } = useQuizGameSocket(quizId, hostname);
 
-  useEffect(() => {
-    socket.emit(
-      "select_quiz_and_create_game",
-      { quizId, hostname },
-
-      (response: any) => {
-        if (response.error) {
-          console.error("Error start multiplayer mode:", response.error);
-          return;
-        }
-
-        if (gameCode === "") {
-          setGameCode(response.gameCode);
-        }
-      },
-    );
-  }, []);
-
-  return <Lobby gameCode={gameCode} />;
+  return <Lobby gameCode={gameCode} players={players} />;
 }
