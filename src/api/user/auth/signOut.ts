5c62df74731180fa1b5113308cead71e68ba3883
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-02-06 14:36:37 +0700

    #FEAT-059: handle non existed session signout (#122)
    * chore
    
    * feat: handle non-existed records
    
    * fix(pkg): remove unused

--- src/api/user/auth/signOut.ts
+++ src/api/user/auth/signOut.ts
@@ -1,6 +1,9 @@
 import { QUERY_URL } from "@/api/constant"
+import Cookies from "universal-cookie";
 
 export async function signOut(token: string) {
+    const cookies = new Cookies(null, { path: "/" });
+
     const response = await fetch(`${QUERY_URL}/auth.signOut`, {
         method: "POST",
         headers: {
@@ -13,6 +16,11 @@ export async function signOut(token: string) {
 
     if (!response.ok) {
         const res = await response.json()
+        if (res.error.message.includes("Record to delete does not exist.")) {
+            cookies.remove("token");
+            cookies.remove("userEmail");
+            return window.location.href = "/auth/accounts/signin";
+        }
         throw new Error(res.error || "Unknown Error")
     }
 
