e728ece5df7125fdeb3767957533763b1f239d7c
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-02-06 17:14:56 +0700

    #FEAT-057: copy to clipboard button (#123)
    * chore
    
    * feat: handle non-existed records
    
    * feat: copy to clipboard function
    
    * feat: display tooltip and change icon
    
    * fix: remove pnpm version

--- src/components/Game/Lobby/Pin.tsx
+++ src/components/Game/Lobby/Pin.tsx
@@ -1,6 +1,19 @@
-import { Clipboard } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Clipboard, ClipboardCheck } from "lucide-react";
+import { useState } from "react";
+
+function CopyToClipboard(pin: string) {
+  navigator.clipboard.writeText(pin);
+}
 
 export default function Pin({ pin }: { pin: string }) {
+  const [copied, setCopied] = useState<boolean>(false);
+
   return (
     <div
       data-testid="pin"
@@ -11,7 +24,27 @@ export default function Pin({ pin }: { pin: string }) {
         <p className="text-6xl w-60 h-18 font-extrabold tracking-widest text-primary">
           {pin}
         </p>
-        <Clipboard className="text-black" />
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              {copied ? (
+                <ClipboardCheck className="text-green-500" />
+              ) : (
+                <Clipboard
+                  onClick={() => {
+                    CopyToClipboard(pin);
+                    setCopied(true);
+                  }}
+                  className="text-black"
+                />
+              )}
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{copied ? "Copied" : "Copy"}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </div>
   );
