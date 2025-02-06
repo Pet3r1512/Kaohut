dd360c8fcde02b9fb4e425246b9e7acc99f046a7
Peter Pham <pttp15122002@gmail.com>
2025-02-06 19:08:05 +0700

    feat(host): game play

--- src/components/Game/Lobby/_index.tsx
+++ src/components/Game/Lobby/_index.tsx
@@ -1,7 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import LoadingScreen from "@/components/LoadingScreen";
 import Pin from "./Pin";
 import Settings from "./Settings";
 import Waiting from "./Waiting";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@radix-ui/react-tooltip";
+import { useSocket } from "@/context/SocketContext";
+import { useState } from "react";
+import QuestionCard from "../Play/QuestionCard";
 
 export interface Player {
   id: string;
@@ -12,10 +22,61 @@ export interface Player {
 export default function Lobby({
   gameCode,
   players,
+  quiz,
 }: {
   gameCode: string;
   players: Player[];
+  quiz: any;
 }) {
+  const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
+  const [currentQuestion, setCurrentQuestion] = useState<any>(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(1);
+  const [answerState, setAnswerState] = useState<
+    "correct" | "incorrect" | "pending" | null
+  >(null);
+  const [score, setScore] = useState<number>(0);
+  const socket = useSocket();
+
+  const handleStartGame = () => {
+    if (!socket) {
+      return;
+    }
+
+    socket.emit(
+      "start_game",
+      { gameCode },
+      (response: { success: boolean; firstQuestion: any; error: any }) => {
+        if (response.error) {
+          console.log(response.error);
+          return;
+        }
+        setIsGameStarted(true);
+        setCurrentQuestion(response.firstQuestion);
+        console.log(response.firstQuestion);
+      },
+    );
+  };
+
+  const answerQuestion = (answerIndex: number) => {
+    if (!socket) {
+      return;
+    }
+
+    socket.emit(
+      "answer_question",
+      { gameCode, answerIndex },
+      (response: { isCorrect: boolean; nextQuestion: any; error: any }) => {
+        if (response.error) {
+          console.log(response.error);
+          return;
+        }
+        setCurrentQuestion(response.nextQuestion);
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setAnswerState("correct");
+      },
+    );
+  };
+
   return (
     <section className="p-5 flex flex-col h-screen w-screen bg-gradient-to-br from-[#d9a7c7] via-[#e1eec3] to-[#fffcdc]">
       {gameCode === "" ? (
@@ -25,8 +86,45 @@ export default function Lobby({
         </div>
       ) : (
         <>
-          <Pin pin={gameCode} />
-          <Waiting players={players} />
+          <div className="w-fit flex flex-col items-center justify-center mx-auto gap-y-5">
+            <Pin pin={gameCode} />
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button
+                    disabled={players.length < 2 ? true : false}
+                    className={`text-white font-bold py-3 px-5 rounded-2xl ${players.length < 2 ? "cursor-not-allowed bg-gray-500" : "bg-green-500"}`}
+                    onClick={handleStartGame}
+                  >
+                    Start Game
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className="bg-gray-300 px-1.5 py-1 rounded-lg">
+                    {players.length < 2
+                      ? "Need 2 players to start the game"
+                      : ""}
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+          {!isGameStarted ? (
+            <Waiting players={players} />
+          ) : (
+            <QuestionCard
+              question={currentQuestion}
+              onAnswer={answerQuestion}
+              answerState={answerState}
+              questionOrder={{
+                current: currentQuestionIndex,
+                total: quiz.quiz.result.data.quiz.length,
+              }}
+              duration={quiz.quiz.result.data.quiz.time}
+              score={score}
+              setScore={setScore}
+            />
+          )}
           <Settings players={players} />
         </>
       )}
